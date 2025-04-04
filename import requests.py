c12df6f94b957ef1b2f5381cd5fcd733c0b5c7b3
ffddrs <2469871236@qq.com>
2025-04-04 16:11:53 +0800

    添加爬取top25页面html代码的函数

--- import requests.py
+++ import requests.py
@@ -1,1 +1,21 @@
+# -*- codinig: utf-8 -*-
 import requests
+import random
+import time
+import string
+from bs4 import BeautifulSoup
+import pymysql
+import re
+
+baseurl="https://movie.douban.com/top250"
+def crawl_top25():
+    header = {  
+        "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36",
+        "Cookie": "bid=%s" % "".join(random.sample(string.ascii_letters + string.digits, 11))
+    }
+    response_top25=requests.get(baseurl,headers=header)
+    time.sleep(random.randint(3,5))
+    response_top25.encoding='utf-8'
+    print(response_top25.text)
+
+crawl_top25()
