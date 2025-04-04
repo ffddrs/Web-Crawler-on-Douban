e7f9b9ed99bdb08bb97d67d4714a2c19ac119961
ffddrs <2469871236@qq.com>
2025-04-04 16:46:31 +0800

    完成 crawl_top25函数 功能：爬取top25主页的每个电影的详情页链接，并返回一个列表

--- import requests.py
+++ import requests.py
@@ -8,14 +8,38 @@ import pymysql
 import re
 
 baseurl="https://movie.douban.com/top250"
+
 def crawl_top25():
     header = {  
         "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36",
         "Cookie": "bid=%s" % "".join(random.sample(string.ascii_letters + string.digits, 11))
     }
-    response_top25=requests.get(baseurl,headers=header)
-    time.sleep(random.randint(3,5))
-    response_top25.encoding='utf-8'
-    print(response_top25.text)
+    try:
+        response_top25=requests.get(baseurl,headers=header)
+        response_top25.encoding='utf-8'
+        soup_top25=BeautifulSoup(response_top25.text,'html.parser')
+        pointer=soup_top25.find('h1',string='豆瓣电影 Top 250')
+        if not pointer:
+            pointer=[]
+        target_div=pointer.find_parent('div',{'id':'content'})\
+                          .find('div',{'class':'article'})
+        if not target_div:
+            target_div=[]
+        try:
+            movies=target_div.ol.find_all('li')
+        except:
+            movies=[]
+        for movie in movies:
+            movie=movie.find('div',{'class':'hd'})\
+                       .find('a')\
+                       .get('href')
+        return movies
+    except Exception as e:
+        print(f"抓取失败: {e}")
+        return []
+
+        
 
 crawl_top25()
+
+
